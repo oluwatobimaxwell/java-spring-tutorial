79154161a5c7da79de1e85cf70ba5b1e7d659352
Abbey <abbeylincholn4real@gmail.com>
2024-11-18 20:59:40 +0000

    add bulbs endpoint

--- src/main/java/com/family_members/app/HelloController.java
+++ src/main/java/com/family_members/app/HelloController.java
@@ -6,11 +6,12 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDateTime;
 import java.util.*;
 
-@CrossOrigin(origins = "http://localhost:3002")
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api")
 public class HelloController {
     private List<Map<String, String>> users = new ArrayList<>();
+
 
     public HelloController(){
         Map<String, String> tobi = new HashMap<>();
@@ -25,8 +26,17 @@ public class HelloController {
         abbey.put("address", "Machester, GB");
         abbey.put("id", "09876556");
 
+        Map<String, String> moyin = new HashMap<>();
+        moyin.put("name", "Moyin Sholanke");
+        moyin.put("school", "Futminna University");
+        moyin.put("address", "Abuja, NG");
+        moyin.put("id", "84847737");
+
+
         users.add(tobi);
         users.add(abbey);
+        users.add(moyin);
+
     }
 
     public Optional<Map<String, String>> _getUserById(String id) {
@@ -61,4 +71,25 @@ public class HelloController {
         return user.map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
+
+
+
+    @GetMapping("/bulbs")
+    public Map<String, Boolean> bulbs(){
+
+        Random random = new Random();
+
+        Map<String, Boolean> bulbs = new HashMap<>();
+        bulbs.put("bulb1", random.nextBoolean());
+        bulbs.put("bulb2", random.nextBoolean());
+        bulbs.put("bulb3", random.nextBoolean());
+        bulbs.put("bulb4", random.nextBoolean());
+
+        return bulbs;
+
+
+    }
+
+
+
 }
